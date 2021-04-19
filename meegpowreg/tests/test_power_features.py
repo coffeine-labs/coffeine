030019d56c147fb1e6b310d883cfd3b9ef1fe57a
Apolline Mellot <apolline.mellot@inria.fr>
2021-04-19 10:10:25 +0200

    mini fixes

--- meegpowreg/tests/test_power_features.py
+++ meegpowreg/tests/test_power_features.py
@@ -13,7 +13,7 @@ fbands = {'alpha': (8.0, 15.0), 'beta': (15.0, 30.0)}
 def test_compute_features_raw():
     raw = mne.io.read_raw_fif(raw_fname, verbose=False)
     raw = raw.copy().crop(0, 200).pick(
-        list(range(2)) + list(range(330, 333))  # take some MEG and EEG
+        [0, 1, 330, 331, 332]  # take some MEG and EEG
     )
     raw.info.normalize_proj()
     features, res = compute_features(raw, fbands=fbands)
@@ -22,7 +22,7 @@ def test_compute_features_raw():
     n_fb = len(fbands)
     assert (
         set(features.keys()) ==
-        set(['psds', 'freqs', 'covs', 'xfreqcovs', 'xfreqcorrs', 'cospcovs'])
+        {'psds', 'freqs', 'covs', 'xfreqcovs', 'xfreqcorrs', 'cospcovs'}
     )
     assert features['psds'].shape == (n_channels, n_freqs)
     assert features['covs'].shape == (n_fb, n_channels, n_channels)
@@ -36,7 +36,7 @@ def test_compute_features_raw():
 def test_compute_features_epochs():
     raw = mne.io.read_raw_fif(raw_fname, verbose=False)
     raw = raw.copy().crop(0, 200).pick(
-        list(range(2)) + list(range(330, 333))  # take some MEG and EEG
+        [0, 1, 330, 331, 332]  # take some MEG and EEG
     )
     raw.info.normalize_proj()
     events = mne.make_fixed_length_events(raw, id=3000,
@@ -52,7 +52,7 @@ def test_compute_features_epochs():
     n_fb = len(fbands)
     assert (
         set(features.keys()) ==
-        set(['psds', 'freqs', 'covs', 'xfreqcovs', 'xfreqcorrs', 'cospcovs'])
+        {'psds', 'freqs', 'covs', 'xfreqcovs', 'xfreqcorrs', 'cospcovs'}
     )
     assert features['psds'].shape == (n_channels, n_freqs)
     assert features['covs'].shape == (n_fb, n_channels, n_channels)
