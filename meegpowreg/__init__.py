a9ea3950d03bbe6874260217c21bd4008cffe66d
Denis A. Engemann <denis-alexander.engemann@inria.fr>
2021-04-21 21:07:20 +0200

    first wave of renaming

--- meegpowreg/__init__.py
+++ meegpowreg/__init__.py
@@ -21,10 +21,10 @@
 
 __version__ = '0.1.dev'
 
-from .featuring import Riemann, Diag, LogDiag, ExpandFeatures, NaiveVec, RiemannSnp  # noqa
+from .covariance_transformers import Riemann, Diag, LogDiag, ExpandFeatures, NaiveVec, RiemannSnp  # noqa
 
 from .pipelines import make_pipelines  # noqa
 
 from .power_features import compute_features  # noqa
 
-from .spfiltering import ProjIdentitySpace, ProjCommonSpace, ProjLWSpace, ProjRandomSpace, ProjSPoCSpace  # noqa
+from .spatial_filters import ProjIdentitySpace, ProjCommonSpace, ProjLWSpace, ProjRandomSpace, ProjSPoCSpace  # noqa
