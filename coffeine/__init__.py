6fbee2f66adc33f0ff6970bbdb7b70ceceea9061
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-12-14 16:19:29 +0100

    bump version to 0.1

--- coffeine/__init__.py
+++ coffeine/__init__.py
@@ -19,7 +19,7 @@
 # Dev branch marker is: 'X.Y.devN' where N is an integer.
 #
 
-__version__ = '0.1.dev0'
+__version__ = '0.1'
 
 from .covariance_transformers import Riemann, Diag, LogDiag, ExpandFeatures, NaiveVec, RiemannSnp  # noqa
 
